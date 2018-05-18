08e4ee904fd50926aa3d4dff94b8feeb39d5bdbc
Sebastian Pietraszek <zaimatsu@gmail.com>
2018-05-18 10:53:19 +0000

    [#5] added token authentication

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var async = require("async");
 var express = require("express");
 
 var bodyParser = require("body-parser");
-
 var request = require("request");
+var _ = require("lodash");
 
 var GameManager = require("./GameManager.js");
 var DefaultGameResponseProvider = require("./DefaultGameResponseProvider.js");
@@ -19,12 +19,25 @@ router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(express.static('public'));
 
+if(_.isEmpty(process.env.VERIFICATION_TOKEN)) {
+  console.error("The server could not start. Set VERIFICATION_TOKEN enviromental variable first.");
+  process.exit(1);
+}
+
 var defaultGameResponseProvider = new DefaultGameResponseProvider();
 var phraseValidator = new PhraseValidator();
 
 var gameManager = new GameManager(defaultGameResponseProvider, phraseValidator);
 
 router.post('/', function(req, res) {
+  var token = req.body.token;
+  
+  if(!_.isEqual(token, process.env.VERIFICATION_TOKEN)) {
+    res.status(401);
+    res.send();
+    return;
+  }
+  
   var channelId = req.body.channel_id;
   var userInput = req.body.text.toUpperCase();
   var user = req.body.user_name;
