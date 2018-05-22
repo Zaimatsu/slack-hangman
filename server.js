f9d49e851c4557ddf7ef14299e44f561048b757e
Zaimatsu <Zaimatsu@gmail.com>
2018-05-22 23:32:23 +0200

    Upgraded express version. Added other environmental variables verification.

--- server.js
+++ server.js
@@ -1,62 +1,70 @@
+var _ = require("lodash");
+var bodyParser = require("body-parser");
+var express = require("express");
+var fs = require("fs");
 var http = require("http");
 var https = require("https");
 var path = require("path");
-
-var express = require("express");
-
-var bodyParser = require("body-parser");
 var request = require("request");
-var _ = require("lodash");
-const url = require("url");
-const fs = require("fs");
+var url = require("url");
 
-var GameManager = require("./GameManager");
 var DefaultGameResponseProvider = require("./DefaultGameResponseProvider");
+var GameManager = require("./GameManager");
 var PhraseValidator = require("./PhraseValidator");
 var SlackResponseSender = require("./SlackResponseSender");
 
-var router = express();
-var httpRouter = express();
+var app = express();
+var httpApp = express();
 
-const options = {
+var sslOptions = {
     key: fs.readFileSync('/etc/letsencrypt/live/zaimatsu.tk/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/zaimatsu.tk/cert.pem'),
     ca: fs.readFileSync('/etc/letsencrypt/live/zaimatsu.tk/chain.pem')
 };
 
-var server = https.createServer(options, router);
+var server = https.createServer(sslOptions, app);
+var httpServer = http.createServer(httpApp);
 
-var httpServer = http.createServer(httpRouter);
-
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: true }));
-router.use(express.static("public"));
-
-httpRouter.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 if (_.isEmpty(process.env.VERIFICATION_TOKEN)) {
-    // TODO add verification with other env vars
     console.error("[SERVER] The server could not start. Set VERIFICATION_TOKEN enviromental variable first.");
+    process.exit(1);
+}
+
+if (_.isEmpty(process.env.CLIENT_ID)) {
+    console.error("[SERVER] The server could not start. Set CLIENT_ID enviromental variable first.");
+    process.exit(1);
+}
+
+if (_.isEmpty(process.env.CLIENT_SECRET)) {
+    console.error("[SERVER] The server could not start. Set CLIENT_SECRET enviromental variable first.");
+    process.exit(1);
+}
+
+if (_.isEmpty(process.env.SOCKET_TOKEN)) {
+    console.error("[SERVER] The server could not start. Set SOCKET_TOKEN enviromental variable first.");
     process.exit(1);
 }
 
 var defaultGameResponseProvider = new DefaultGameResponseProvider();
 var phraseValidator = new PhraseValidator();
 var slackResponseSender = new SlackResponseSender();
-
 var gameManager = new GameManager(defaultGameResponseProvider, phraseValidator);
 
-httpRouter.get("/", function (req, res) {
+httpApp.get("/", function (req, res) {
     console.log("[HTTP SERVER] GET '/'", req.ip, JSON.stringify(req.query));
     res.redirect("https://github.com/Zaimatsu/slack-hangman");
 });
 
-router.get("/", function (req, res) {
+app.get("/", function (req, res) {
     console.log("[HTTPS SERVER] GET '/'", req.ip, JSON.stringify(req.body));
     res.redirect("https://github.com/Zaimatsu/slack-hangman");
 });
 
-router.post("/", function (req, res) {
+app.post("/", function (req, res) {
     console.log("[HTTPS SERVER] POST '/'", req.ip, JSON.stringify(req.body));
     var token = req.body.token;
 
@@ -75,7 +83,7 @@ router.post("/", function (req, res) {
     slackResponseSender.send(slackResponse, req, res);
 });
 
-router.get("/oauth", function (req, res) {
+app.get("/oauth", function (req, res) {
     var query = url.parse(req.url, true).query;
     var code = query.code;
 
@@ -126,12 +134,12 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(process.env.PORT || 443, process.env.IP || "0.0.0.0", function () {
+server.listen(443, "0.0.0.0", function () {
     var addr = server.address();
     console.log("[HTTPS SERVER] Listening at", addr.address + ":" + addr.port);
 });
 
-httpServer.listen(process.env.PORT || 80, process.env.IP || "0.0.0.0", function () {
+httpServer.listen(80, "0.0.0.0", function () {
     var addr = httpServer.address();
     console.log("[HTTP SERVER] Listening at", addr.address + ":" + addr.port);
 });
